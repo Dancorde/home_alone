681ca844b0c8afd7c181e625363873ca83142521
Danilo Cordeiro <dancorde@gmail.com>
2018-06-21 09:56:27 -0300

    Seed criada

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,31 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Place.create!(
-  adress: "R. São Paulo, 1079 - Vila Monteiro (Gleba I), São Carlos - SP, 13560-340, Brazil",
+  address: "R. São Paulo, 1079 - Vila Monteiro (Gleba I), São Carlos - SP, 13560-340, Brazil",
   latitude: -22.0173682855448,
   longitude: -47.8857599,
   descricao: "2 GARAGENS, 2 SALAS COM PISO PORCELANATO, LAVABO AZULEJADO ATÉ O TETO, 1 COZINHA AZULEJADA ATÉ O TETO, 3 DORMITÓRIOS COM PISO PORCELANATO SENDO 1 SUITE, 1 WC, ÁREA DE SERVIÇO AZULEJADA ATÉ O TETO, DESPENSA, SALÃO DE FESTAS, QUADRA DE ESPORTES, PLAY GROUND, CHURRASQUEIRA, PORTÃO ELETRÔNICO. ENTREGA PREVISTA PARA JULHO DE 2018."
 )
 Place.create!(
-  adress: "R. Afonso Botelho de Abreu Sampaio, 496 - Parque Santa Felicia Jardim, São Carlos - SP, 13563-307, Brazil",
+  address: "R. Afonso Botelho de Abreu Sampaio, 496 - Parque Santa Felicia Jardim, São Carlos - SP, 13563-307, Brazil",
   latitude: -21.9964821315674,
   longitude: -47.9173267698364,
   descricao: "AO LADO DO N° 789 LOTE 16 QUADRA 98 ÁREA DO TERRENO: 10X30 ÁREA TOTAL: 300M²"
 )
 Place.create!(
-  adress: R. Paulo Toyama Riuji, 150 - Lot. Hab. Sao Carlos 1, São Carlos - SP, 13563-050, Brazil,
+  address: "R. Paulo Toyama Riuji, 150 - Lot. Hab. Sao Carlos 1, São Carlos - SP, 13563-050, Brazil",
   latitude: -21.9932755517871,
   longitude: -47.9226588774902,
   descricao: "1 SALA COM PISO FRIO, 1 COZINHA AZULEJADA ATÉ O TETO, 1 COPA, 5 DORMITÓRIOS COM PISO FRIO SENDO 5 SUITES, 1 ÁREA DE SERVIÇO, ESCRITÓRIO, PISCINA, CHURRASQUEIRA, QUINTAL, 1 CASA CASEIRO COM 1 SALA 1 COPA 1 COZINHA 2 DORMITÓRIOS 1 WC , CÂMERA E ALARME, POMAR GRANDE. ACEITA IMÓVEIS."
 )
 Place.create!(
-  adress: "R. Jorge Tibiriçá, 338 - Jardim Centenario, São Carlos - SP, 13564-110, Brazil",
+  address: "R. Jorge Tibiriçá, 338 - Jardim Centenario, São Carlos - SP, 13564-110, Brazil",
   latitude: -21.9979393135239	,
   longitude: -47.9051263423279,
   descricao: "DEPÓSITO + TERRENO COM ALICERCE E MURO. LOTE 26 QUADRA G ÁREA DO TERRENO: 11X30 ÁREA TOTAL: 330M² ÁREA CONSTRUÍDA: 70M²"
 )
 Place.create!(
-  adress: "R. São Paulo, 1096 - Vila Monteiro (Gleba I), São Carlos - SP, 13560-340, Brazil",
+  address: "R. São Paulo, 1096 - Vila Monteiro (Gleba I), São Carlos - SP, 13560-340, Brazil",
   latitude: -22.0172588752698,
   longitude: -47.8857384423279,
   descricao: "EM CONSTRUÇÃO. APARTAMENTO EM CONSTRUÇÃO COM 164,88M². 2 GARAGENS COBERTAS, 2 SALAS COM PISO PORCELANATO, LAVABO AZULEJADO ATÉ O TETO, 1 COZINHA AZULEJADA ATÉ O TETO, 4 DORMITÓRIOS COM PISO PORCELANATO SENDO 2 SUITES, 1 WC, ÁREA DE SERVIÇO AZULEJADA ATÉ O TETO, DESPENSA, SALÃO DE FESTAS, QUADRA DE ESPORTES, PLAY GROUND, CHURRASQUEIRA, PORTÃO ELETRÔNICO, QUINTAL."
